d8a9ebb805fb5a772beab6850d747a1750395e1e
Sayyora <ya.sofochka2010@yandex.ru>
2025-09-07 18:02:26 +1000

    add fixtures to test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number():
@@ -11,3 +11,25 @@ def test_get_mask_card_number():
 def test_get_mask_account():
     assert get_mask_account(1234567890123456) == "**3456"
     assert get_mask_account(123) == "Некорректный ввод"
+
+
+# Фикстура для предоставления тестовых данных
+@pytest.fixture
+def test_data_get_mask_card_number():
+    return [(1234567890123456, "1234 56** **** 3456"), (12345678, "Некорректный ввод")]
+
+
+@pytest.fixture
+def test_data_get_mask_account():
+    return [(1234567890123456, "**3456"), (123, "Некорректный ввод")]
+
+
+# Тест с использованием фикстур и параметризации
+@pytest.mark.parametrize("input_data, expected", test_data_get_mask_card_number)
+def test_get_mask_card_number(input_data, expected):
+    assert get_mask_card_number(input_data) == expected
+
+
+@pytest.mark.parametrize("input_data, expected", test_data_get_mask_account)
+def test_get_mask_account(input_data, expected):
+    assert get_mask_account(input_data) == expected
